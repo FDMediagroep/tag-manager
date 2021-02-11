ec5d52caf7cde558040282dcfc9860fe73cfe854
Willem Liu <willem.liu@fdmediagroep.nl>
2021-02-11 16:07:41 +0100

    Error message handling

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { csrfToken } from 'next-auth/client';
 import { TextInput } from '@fdmg/design-system/components/input/TextInput';
 import { Button } from '@fdmg/design-system/components/button/Button';
 import styles from './signin.module.scss';
 
 function SignIn({ csrfToken }) {
+    const [error, setError] = useState('');
+
+    useEffect(() => {
+        const params = new URLSearchParams(window.location.search);
+        const errorType = params.get('error');
+        if (errorType) {
+            setError(errorType);
+            if (errorType === 'CredentialsSignin') {
+                setError('Credentials incorrect');
+            }
+        }
+    }, []);
+
     return (
         <section className={styles.signin}>
             <form method="post" action="/api/auth/callback/credentials">
+                {error && <p className={styles.error}>{error}</p>}
                 <input
                     name="csrfToken"
                     type="hidden"
